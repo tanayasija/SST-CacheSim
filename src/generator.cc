809fbe36a168a3d1dd01a980827354364fe9aee1
xuanpeng <thkwrldletsgo@gmail.com>
2022-11-11 16:39:01 -0500

    skeleton of generator.h

---
+++ src/generator.cc
@@ -0,0 +1,26 @@
+// Copyright 2009-2022 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2022, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// of the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+
+// This include is ***REQUIRED*** 
+// for ALL SST implementation files
+#include "sst_config.h"
+
+#include "basicEvent.h"
+#include "basicEvent.h"
+
+
+using namespace SST;
+using namespace SST::XTSimGenerator;
