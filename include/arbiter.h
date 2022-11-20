e7235f61dd65719d16642eeb3cabc534463b85f7
xuanpeng <thkwrldletsgo@gmail.com>
2022-11-20 18:34:25 -0500

    completed implementation of arbiter

---
+++ include/arbiter.h
@@ -0,0 +1,106 @@
+#ifndef _XTSIM_ARBITER_H
+#define _XTSIM_ARBITER_H
+
+#include <sst/core/component.h>
+#include <sst/core/link.h>
+#include "event.h"
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <list>
+using std::vector;
+using std::string;
+using std::queue;
+using std::list;
+
+
+namespace SST {
+namespace xtsim {
+
+enum class ArbPolicy {
+	FIFO = 0, 
+	RR = 1
+};
+
+class XTSimArbiter : public SST::Component {
+public:
+
+/*
+ *  SST Registration macros register Components with the SST Core and 
+ *  document their parameters, ports, etc.
+ *  SST_ELI_REGISTER_COMPONENT is required, the documentation macros
+ *  are only required if relevant
+ */
+    // REGISTER THIS COMPONENT INTO THE ELEMENT LIBRARY
+    SST_ELI_REGISTER_COMPONENT(
+        XTSimArbiter,                       // Component class
+        "xtsim",         // Component library (for Python/library lookup)
+        "XTSimArbiter",                     // Component name (for Python/library lookup)
+        SST_ELI_ELEMENT_VERSION(1,0,0), // Version of the component (not related to SST version)
+        "Simple Bus Arbiter Component",        // Description
+        COMPONENT_CATEGORY_UNCATEGORIZED    // Category
+    )
+
+    // Document the parameters that this component accepts
+    // { "parameter_name", "description", "default value or NULL if required" }
+    SST_ELI_DOCUMENT_PARAMS(
+        { "processorNum", "How many processor we have.", NULL},
+        { "arbPolicy",    "Which arbitration policy we use.", NULL}
+    )
+
+    // Document the ports that this component has
+    // {"Port name", "Description", { "list of event types that the port can handle"} }
+    SST_ELI_DOCUMENT_PORTS(
+        {"arbitrationPort",  "Link to another component", { "xtsim.ArbEvent", ""} }
+    )
+    
+    // Optional since there is nothing to document - see statistics example for more info
+    SST_ELI_DOCUMENT_STATISTICS( )
+
+    // Optional since there is nothing to document - see SubComponent examples for more info
+    SST_ELI_DOCUMENT_SUBCOMPONENT_SLOTS( )
+
+// Class members
+
+    // Constructor. Components receive a unique ID and the set of parameters that were assigned in the Python input.
+    XTSimArbiter(SST::ComponentId_t id, SST::Params& params);
+    
+    // Destructor
+    ~XTSimArbiter();
+
+private:
+	// fifo queue
+	queue<ArbEvent> fifoQueue;
+
+	// for round robin
+	size_t nextPid = 0;
+	list<ArbEvent> rrList;
+	list<ArbEvent>::iterator acIter = nullptr;
+
+	void sendEvent();
+
+	// event handler
+	void handleEvent(SST::Event* ev);
+
+	// selector for Round-robin
+	void getNext();
+
+    // SST Output object, for printing, error messages, etc.
+    SST::Output* out;
+
+	// number of processors
+	size_t processorNum;
+
+	// ArbPolicy
+	ArbPolicy arbPolicy;
+
+    // Links
+	vector<SST::Link*> links;
+	
+};
+
+}
+}
+#endif
---
+++ src/arbiter.cc
@@ -0,0 +1,143 @@
+// Copyright 2009-2022 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2022, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// of the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+// This include is ***REQUIRED***
+// for ALL SST implementation files
+#include <stdio.h>
+#include "sst_config.h"
+
+#include "./include/arbiter.h"
+
+using namespace SST;
+using namespace SST::xtsim;
+
+/*
+ * During construction the XTSimGenerator component should prepare for simulation
+ * - Read parameters
+ * - Configure link
+ * - Register its clock
+ * - Send event for every clock ticking
+ * - Register statistics
+ */
+XTSimArbiter::XTSimArbiter(ComponentId_t id, Params &params) : Component(id) {
+
+    // SST Output Object
+    // Initialize with
+    // - no prefix ("")
+    // - Verbose set to 1
+    // - No mask
+    // - Output to STDOUT (Output::STDOUT)
+
+    // read configuration
+    out = new Output("", 1, 0, Output::STDOUT);
+
+    // Get parameter from the Python input
+    // bool found;
+    processorNum = params.find<size_t>("processorNum");
+	arbPolicy = params.find<ArbPolicy>("arbPolicy");
+
+    // Tell the simulation not to end until we're ready
+    // registerAsPrimaryComponent();
+    // primaryComponentDoNotEndSim();
+
+    // configure our link with a callback function that will be called whenever an event arrives
+    // Callback function is optional, if not provided then component must poll the link
+	links.resize(processorNum);
+	for(int i = 0 ;i < processorNum; ++i){
+		string portName = "arbitrationPort" + i;
+		links[i] = configureLink(portName, new Event::Handler<XTSimArbiter>(this, &XTSimArbiter::handleEvent));
+		sst_assert(links[i], CALL_INFO, -1, "Error in %s: Link configuration failed\n", getName().c_str());
+	}
+
+    // Make sure we successfully configured the links
+    // Failure usually means the user didn't connect the port in the input file
+
+    // set our clock. The simulator will call 'clockTic' at a 1GHz frequency
+    // registerClock("1GHz", new Clock::Handler<XTSimArbiter>(this, &XTSimArbiter::clockTic));
+
+
+    // establish the link between corresponding cache
+	// link = configureLink("port");
+
+    // This simulation will end when we have sent 'eventsToSend' events and received a 'LAST' event
+    // lastEventReceived = false;
+
+    // Register the statistic to link our variable to the documented statistic name
+    // bytesReceived = registerStatistic<uint64_t>("EventSizeReceived");
+}
+
+void XTSimArbiter::getNext(){
+	rrList.erase(acIter);
+	while(true){
+		for(auto it = rrList.begin(); it != rrList.end(); ++it){
+			if(it->pid = nextPid){
+				acIter = it;
+				nextPid ++;
+				return;
+			}
+		}
+		nextPid ++;
+	}
+}
+
+void XTSimArbiter::handleEvent(SST::Event* ev){
+	ArbEvent* arbEvent = dynamic_cast<ArbEvent*>(ev);
+	printf("arbiter received event with type: %d\n", arbEvent->event_type);
+
+	// if receiving a release event
+	if(arbEvent->event_type == ARB_EVENT_TYPE::RL){
+		if(arbPolicy == ArbPolicy::FIFO){
+			fifoQueue.pop();
+		}
+		else{
+			getNext();
+		}
+		sendEvent();
+		return;
+	}
+
+	// else if receiving an acquire event
+	if(arbPolicy == ArbPolicy::FIFO){
+		fifoQueue.push(*arbEvent);
+		if(fifoQueue.size() == 1){
+			sendEvent();
+		}
+	}
+	else if(arbPolicy == ArbPolicy::RR){
+		rrList.push_back(*arbEvent);
+		if(rrList.size() == 1){
+			sendEvent();
+		}
+	}
+}
+
+void XTSimArbiter::sendEvent(){
+	if(arbPolicy == ArbPolicy::FIFO){
+		ArbEvent ev = fifoQueue.front();
+		links[ev.pid]->send(&ev);
+	}
+	else{
+		ArbEvent ev = *acIter;
+		links[ev.pid]->send(&ev);
+	}
+}
+
+/*
+ * Destructor, clean up our output
+ */
+XTSimArbiter::~XTSimArbiter()
+{
+    delete out;
+}
