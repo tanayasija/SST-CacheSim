831fffdeda12cbd1383cd6deaca17588ede9bf0d
Tanay Asija <tasija@andrew.cmu.edu>
2022-11-26 15:58:16 -0500

    Multi threaded test

--- tests/generatorNcache.py
+++ tests/generatorNcache.py
@@ -4,50 +4,67 @@ import os
 
 print("current directory:" + os.getcwd())
 
+num_processors = 4
+trace_name = "sum_"
+
 ### Create the components
-cache = sst.Component("cache", "xtsim.cache")
-generator = sst.Component("generator", "xtsim.XTSimGenerator")
+
 bus = sst.Component("bus", "xtsim.XTSimBus")
 arbiter = sst.Component("arbiter", "xtsim.XTSimArbiter")
 
-
-### Parameterize the components.
-# Run 'sst-info simpleElementExample.example0' at the command line 
-# to see parameter documentation
-generatorParams = {
-        "generatorID" : 0,    # Required parameter, error if not provided
-        "traceFilePath" : "./traces/pinatrace.out"
-}
-generator.addParams(generatorParams)
-
-cacheParams = {
-        "blockSize" : 64,    # Required parameter, error if not provided
-        "cacheSize" : 16384,       # Optional parameter, defaults to 16 if not provided,
-        "associativity" : 4,
-        "cacheId" : 0
-}
-cache.addParams(cacheParams)
-
 arbiterParams = {
-        "processorNum" : 1,    # Required parameter, error if not provided
+        "processorNum" : num_processors,    # Required parameter, error if not provided
         "arbPolicy" : 0
 }
 arbiter.addParams(arbiterParams)
 
 busParams = {
-        "processorNum" : 1,    # Required parameter, error if not provided
+        "processorNum" : num_processors,    # Required parameter, error if not provided
 }
 bus.addParams(busParams)
 
-### Link the components via their 'port' ports
-link = sst.Link("proc_link")
-link.connect( (cache, "processorPort", "1ns"), (generator, "processorPort", "1ns"))
 
-link = sst.Link("bus_link")
-link.connect( (cache, "busPort", "1ns"), (bus, "busPort_0", "1ns"))
+### Parameterize the components.
+# Run 'sst-info simpleElementExample.example0' at the command line 
+# to see parameter documentation
+caches = []
+generators = []
+procLinks = []
+arbLinks = []
+busLinks = []
+for i in range(num_processors):
+        cache = sst.Component("cache" + str(i), "xtsim.cache")
+        generator = sst.Component("generator" + str(i), "xtsim.XTSimGenerator")
+        caches.append(cache)
+        generators.append(generator)
 
-link = sst.Link("arb_link")
-link.connect( (cache, "arbiterPort", "1ns"), (arbiter, "arbiterPort_0", "1ns"))
+        generatorParams = {
+                "generatorID" : i,    # Required parameter, error if not provided
+                "traceFilePath" : "./traces/" + trace_name + str(i) + ".txt"
+        }
+        generator.addParams(generatorParams)
+
+        cacheParams = {
+                "blockSize" : 64,    # Required parameter, error if not provided
+                "cacheSize" : 16384,       # Optional parameter, defaults to 16 if not provided,
+                "associativity" : 4,
+                "cacheId" : 0
+        }
+        cache.addParams(cacheParams)
+
+        ### Link the components via their 'port' ports
+        proclink = sst.Link("proc_link")
+        proclink.connect( (cache, "processorPort", "1ns"), (generator, "processorPort", "1ns"))
+
+        buslink = sst.Link("bus_link")
+        buslink.connect( (cache, "busPort", "1ns"), (bus, "busPort_" + str(i), "1ns"))
+
+        arblink = sst.Link("arb_link")
+        arblink.connect( (cache, "arbiterPort", "1ns"), (arbiter, "arbiterPort_" + str(i), "1ns"))
+
+        procLinks.append(proclink)
+        busLinks.append(buslink)
+        arbLinks.append(arblink)
 
 ### Enable statistics
 # Limit the verbosity of statistics to any with a load level from 0-7
